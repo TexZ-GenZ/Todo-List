43094dcbb776b68fb9d9a813c58dc376b0b12a6c
Priyanshu Samal <priyanshusamal21@gmail.com>
2024-06-26 21:33:39 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Setup
+
+To set up the project, run the following commands in your terminal:
+
+```sh
+chmod +x setup.sh
+./setup.sh
---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Initialize a new npm project
+npm init -y
+
+# Install Webpack and its CLI
+npm install webpack webpack-cli --save-dev
+
+# Install HtmlWebpackPlugin
+npm install --save-dev html-webpack-plugin
+
+# Install Webpack Dev Server
+npm install --save-dev webpack-dev-server
+
+# Install Webpack Merge
+npm install --save-dev webpack-merge
+
+# Install CSS Loader and Style Loader
+npm install css-loader style-loader --save-dev
+
+echo "All dependencies have been installed. Your project is ready to use Webpack."
